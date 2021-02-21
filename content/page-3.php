3710bd7cd809934d2f61bd73ba861e6954a153e3
Vitaliy Ponomarenko <glitch.coder.256@gmail.com>
2021-02-21 13:04:00 +0200

    Добавлено содержимое страниц 2 и 3
    На ветке develop
    Изменения, которые будут включены в коммит:
    	изменено:      content/page-2.php
    	изменено:      content/page-3.php

--- content/page-3.php
+++ content/page-3.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title>Ветки</title>
+    <link rel="stylesheet" href="/css/main-css.css" />
+</head>
+<body>
+<header>
+    <h1>Работа с ветками.</h1>
+    <nav>
+        <ul>
+            <li><a href="content/page-1.php">Страница 1</a></li>
+            <li><a href="content/page-2.php">Страница 2</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <h2>Страница 3</h2>
+    <p>Содержимое страницы ==> 3 <==</p>
+</main>
+<footer>Копирайт.</footer>
+</body>
+</html>
