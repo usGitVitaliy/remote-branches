e268a4a056bf08724e6673e3b85661929cf556b3
Vitaliy Ponomarenko <glitch.coder.256@gmail.com>
2021-02-21 12:19:45 +0200

    Создание стартовой страницы
    На ветке master
    
    Начальный коммит
    
    Изменения, которые будут включены в коммит:
    	новый файл:    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8"/>
+    <title>Ветки</title>
+    <style>
+        body {
+            box-sizing: border-box;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>Работа с ветками.</h1>
+    <p>В этом сайте рассматривается работа с ветками.</p>
+</body>
+</html>
